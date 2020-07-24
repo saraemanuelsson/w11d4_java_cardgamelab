75f24fb64d8a863fa12bf0a06d86efe732943f2d
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-24 11:54:21 +0100

    Renamed methods in player and added getTotalValueOfHand method

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,44 +2,28 @@ import java.util.ArrayList;
 
 public class Player {
 
-//private ArrayList<Card> dealtCards;
-//private String name;
-//private Boolean winner;
-//
-//  public Player(String name) {
-//    this.name = name;
-//    this.winner = false;
-//    this.dealtCards = new ArrayList<Card>();
-//  }
-//
-//  public String getName() {
-//    return this.name;
-//  }
-//
-//  public Boolean isWinner() {
-//    return this.winner;
-//  }
-//
-//  public ArrayList<Card> getDealtCards() {
-//    return this.dealtCards;
-//  }
-//
-//  public int hasCard() {
-//    return this.dealtCards.size();
-//  }
-//
-//  public void receiveCard(Deck deck) {
-//    Card dealtCard = deck.dealCard();
-//    this.dealtCards.add(dealtCard);
-//  }
-//
-//  public void setWinner(){
-//    this.winner = true;
-//  }
+private ArrayList<Card> handOfCards;
+private String name;
 
-//  public int getValueOfCard(){
-//
-//  }
-  
+  public Player(String name) {
+    this.name = name;
+    this.handOfCards = new ArrayList<Card>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public ArrayList<Card> getHandOfCards() {
+    return this.handOfCards;
+  }
+
+  public int getNumberOfCards() {
+    return this.handOfCards.size();
+  }
+
+  public void receiveCard(Card card) {
+    this.handOfCards.add(card);
+  }
 
 }
